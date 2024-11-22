197eb23190fb49e36df63486fddf80152256233a
Connor Allen <allen.2784@buckeyemail.osu.edu>
2024-11-22 16:01:26 -0500

    Added a name field to the user

--- Final-Project/app/controllers/users_controller.rb
+++ Final-Project/app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :encrypted_password, :role)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation :role)
   end
 end
--- Final-Project/db/schema.rb
+++ Final-Project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_21_020436) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_22_204014) do
   create_table "courses", force: :cascade do |t|
     t.string "teacher"
     t.text "students"
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_21_020436) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "student"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

466ecab809afa6d317fa4dc4b22d97e9c3c0c5a5
Connor Allen <allen.2784@buckeyemail.osu.edu>
2024-11-22 16:10:05 -0500

    Merged after addition of name field to user

--- Final-Project/app/models/user.rb
+++ Final-Project/app/models/user.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -9,6 +8,8 @@ class User < ApplicationRecord
   before_save :set_default_role
 
   has_many :courses
+
+  validates :name, presence:true
 
   validates :email, presence:true, 
   length:{maximum: 50},
@@ -24,32 +25,3 @@ class User < ApplicationRecord
     self.role ||= "student"
   end
 end
-=======
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  # Roles
-  enum role: { student: "student", teacher: "teacher"}
-
-  before_save :set_default_role
-
-  has_many :courses
-  
-  validates :name, presence:true, 
-
-  validates :email, presence:true, 
-  length:{maximum: 50},
-  uniqueness:true,
-  format:{with: /\A\w+\.\d+@\w+\.\w+\Z/i }
-
-  validates :encrypted_password, presence:true,
-  length: {minimum: 6}
-
-  private
-
-  def set_default_role
-    self.role ||= "student"
-  end
-end
->>>>>>> 7c2aa93451ecfab2cf9ef62d486d84dd41b80851

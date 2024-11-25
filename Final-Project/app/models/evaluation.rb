1c408f62c39ff5baf9532edbfbe313d1b7618eb0
Hamilton <hamiltonb63@outlook.com>
2024-11-25 15:35:37 -0500

    added validations to evalutation model

--- Final-Project/app/models/evaluation.rb
+++ Final-Project/app/models/evaluation.rb
@@ -1,4 +1,8 @@
 class Evaluation < ApplicationRecord
   belongs_to :presentation
   belongs_to :user # This links the evaluation to the user who submitted it
+
+  validates :comment, presence: true
+  validates :score, presence: true, numericality: {only_integer: true},
+  comparison: {less_than_or_equal_to: 100}
 end

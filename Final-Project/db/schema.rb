0c2a13bbe9b1691c095cf783ddca5844b0467a76
Brett Emory <emory.19@buckeyemail.osu.edu>
2024-12-02 16:37:21 -0500

    email varification

--- Final-Project/db/schema.rb
+++ Final-Project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_25_211835) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_02_200629) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -45,9 +45,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_25_211835) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "description"
-    t.string "credits"
     t.integer "user_id", null: false
+    t.text "description"
     t.integer "course_id"
     t.index ["course_id"], name: "index_presentations_on_course_id"
     t.index ["user_id"], name: "index_presentations_on_user_id"
@@ -62,7 +61,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_25_211835) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "student"
-    t.string "name", default: "default"
+    t.string "name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
